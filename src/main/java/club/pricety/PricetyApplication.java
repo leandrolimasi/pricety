e27167c92ddff90b21876e5caad6b08408dda52e
leandrolimasi <leandrolimadasilva@gmail.com>
2018-08-03 14:14:58 -0300

    initial commit

---
+++ src/main/java/club/pricety/PricetyApplication.java
@@ -0,0 +1,12 @@
+package club.pricety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricetyApplication.class, args);
+	}
+}
